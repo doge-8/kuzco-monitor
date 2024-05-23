1e5d5e582e6caa93e7f8bca155a2fad04895e387
doge-8 <84656053+doge-8@users.noreply.github.com>
2024-05-23 09:33:24 +0800

    Update kz.py

--- kz.py
+++ kz.py
@@ -5,14 +5,16 @@ import sys
 import subprocess
 
 # 设置参数
-workers = 5  # 工作的数量
+workers = 2  # 工作的数量
 check_interval = 600  # 检测时间间隔，单位：秒
 restart_wait_time = 30  # 重启等待时间，单位：秒
 
 # Discord参数
-discord_webhook_url = "https://discord.com/api/webhooks/"  # 填入你的Discord Webhook URL
-discord_message = "kuzco运行异常，正在重启程序..."  # 填入需要发送的消息内容
-send_discord_notifications = input("输入1以禁用Discord消息推送，输入其他任意值以启用: ") != '1'
+discord_webhook_url = "https://discord.com/api/webhooks/1240579391172640842/qZyCYB_A534AS3A7KdNGrAh-Z6hAFeTdqa2oqqBoKzreyHQ2mM5E27vBQqHS10LUfF-g"  # Discord Webhook URL
+discord_message = "kuzco运行异常，正在重启程序..."  # 发送的消息内容
+send_discord_notifications = input("输入1以禁用Discord消息推送，输入其他任意值以启用: ") != '1'  # 控制消息推送的开关
+
+log_directory = '/var/log/kuzco/'
 
 def count_finish(file_path):
     with open(file_path, 'r') as file:
@@ -25,13 +27,14 @@ def clear_log(file_path):
         file.truncate(0)
 
 def start_kuzco(workers):
-    print("启动1号kuzco中")
-    os.system("kuzco worker start > /root/log.txt 2>&1 &")
-    time.sleep(6)
-    for i in range(2, workers + 1):
-        print(f"启动{i}号kuzco中")
-        os.system("kuzco worker start > /dev/null 2>&1 &")
+    if workers > 0:
+        print("启动1号kuzco中")
+        os.system(f"kuzco worker start > {log_directory}/log.txt 2>&1 &")
         time.sleep(6)
+        for i in range(2, workers + 1):
+            print(f"启动{i}号kuzco中")
+            os.system(f"kuzco worker start > /dev/null 2>&1 &")
+            time.sleep(6)
 
 def send_discord_message():
     if send_discord_notifications:
@@ -43,14 +46,17 @@ def send_discord_message():
 def exit_handler(signal, frame):
     print("\n检测脚本已关闭，清除所有kuzco...")
     os.system("pkill -9 'kuzco'")
-    clear_log('/root/log.txt')
+    clear_log(f'{log_directory}/log.txt')
     sys.exit(0)
 
 def main():
-    file_path = '/root/log.txt'
+    file_path = f'{log_directory}/log.txt'
 
     signal.signal(signal.SIGINT, exit_handler)
     signal.signal(signal.SIGTERM, exit_handler)
+
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
 
     start_kuzco(workers)
 
