218977a6a5836882b8eab6b08436a127256f5f87
doge-8 <84656053+doge-8@users.noreply.github.com>
2024-06-16 14:46:29 +0800

    Update kz.py

--- kz.py
+++ kz.py
@@ -2,18 +2,12 @@ import os
 import signal
 import time
 import sys
-import subprocess
 import re
 
 # 可调用户设置参数
 workers = 3  # 开越多网页控制板Generations计算延迟越高，计算延迟控制在3w左右合适，超过3万会假死！！
 check_interval = 300  # 检测时间间隔，单位：秒
 restart_wait_time = 30  # 重启等待时间，单位：秒
-discord_webhook_url = "https://discord.com/api/webhooks/"  # Discord Webhook URL
-discord_message = "kuzco运行异常，正在重启程序..."  # 发送的消息内容
-
-# 此参数不推荐修改！
-send_discord_notifications = input("输入1以启用Discord消息推送功能，输入其他任意值以禁用: ") == '1'
 
 log_directory = '/var/log/kuzco/'
 
@@ -48,39 +42,18 @@ def clear_all_logs():
             with open(log_file_path, 'w') as file:
                 file.truncate(0)
 
-def update_worker_status():
-    os.system("jq '.status = \"Offline\"' /root/.kuzco/worker.json > /root/.kuzco/worker_temp.json && mv /root/.kuzco/worker_temp.json /root/.kuzco/worker.json")
-
 def start_kuzco(workers, start_suffix):
     if workers > 0:
         for i in range(1, workers + 1):
             print(f"启动{i}号kuzco中")
-            update_worker_status()
             os.system(f"kuzco worker start {start_suffix} > {log_directory}/log{i}.txt 2>&1 &")
-            update_worker_status()
             time.sleep(6)
-
-def send_discord_message():
-    if send_discord_notifications:
-        hostname = subprocess.run("hostname", shell=True, capture_output=True, text=True).stdout.strip()
-        message = f"当前主机：{hostname} - {discord_message}"
-        curl_command = f'curl -H "Content-Type: application/json" -X POST -d \'{{"content": "{message}"}}\' "{discord_webhook_url}"'
-        subprocess.run(curl_command, shell=True)
 
 def exit_handler(signal, frame):
     print("\n检测脚本已关闭，清除所有kuzco...")
     os.system("pkill -9 'kuzco'")
-    update_worker_status()
     clear_all_logs()
     sys.exit(0)
-
-def install_jq_if_not_installed():
-    jq_installed = subprocess.run("which jq", shell=True, stdout=subprocess.PIPE).stdout.decode().strip()
-    if not jq_installed:
-        print("jq 未安装，正在安装...")
-        install_command = "sudo apt-get install -qq jq -y"  # 适用于 Ubuntu/Debian 系统
-        subprocess.run(install_command, shell=True)
-        print("jq 安装完毕，启动kuzco中...")
 
 def main():
     signal.signal(signal.SIGINT, exit_handler)
@@ -88,8 +61,6 @@ def main():
 
     if not os.path.exists(log_directory):
         os.makedirs(log_directory)
-
-    install_jq_if_not_installed()  # 添加安装 jq 的检查
 
     start_kuzco(workers, start_suffix)
 
@@ -108,7 +79,6 @@ def main():
             print("检测到kuzco异常，尝试重启中...")
             os.system("pkill -9 'kuzco'")
             time.sleep(restart_wait_time)
-            send_discord_message()
             start_kuzco(workers, start_suffix)
 
         clear_all_logs()
